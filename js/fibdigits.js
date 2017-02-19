915ca2c5e158221a7efbefaeb3a07274440987ff
blah blah <sa-mm@users.noreply.github.com>
2017-02-19 17:23:41 -0600

    Cleanup answer

--- js/fibdigits.js
+++ js/fibdigits.js
@@ -1,14 +1,11 @@
 function firstFibDigits(digits) {
-  if (digits === 1) {
-    return 1;
-  }
-  var fibArray = [1, 1];
+  var fibArray = [0, 1];
   while (getLast(fibArray).toString().length < digits) {
     var last = getLast(fibArray);
     var penultimate = fibArray[fibArray.length - 2];
     fibArray.push(penultimate + last);
   }
-  return fibArray.length;
+  return fibArray.length - 1;
 }
 
 function getLast(arr) {
